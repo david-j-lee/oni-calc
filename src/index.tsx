31d5f1729b9fe1110f1154960db01b83df0c0526
David Lee <devdavidlee@gmail.com>
2018-08-11 12:50:51 -0700

    web font loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as WebFont from 'webfontloader';
 
 import App from './containers/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+WebFont.load({
+  google: {
+    families: ['Nova Square:400', 'sans-serif'],
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
