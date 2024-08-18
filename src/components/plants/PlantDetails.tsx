5e864409b2e212629b9f3e87680322d8d36d3ac8
David Lee <devdavidlee@gmail.com>
2024-08-17 20:45:21 -0700

    Migrate plant details to new UI

--- src/components/plants/PlantDetails.tsx
+++ src/components/plants/PlantDetails.tsx
@@ -1,9 +1,7 @@
 import ResourceChips from '../resources/ResourceChips';
+import DetailsBase from '../ui/DetailsBase';
+import DetailsSection from '../ui/DetailsSection';
 import IPlant from './../../interfaces/IPlant';
-import { css } from '@emotion/react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { Theme } from '@mui/material/styles';
 import { FC, memo, useMemo } from 'react';
 
 interface IProps {
@@ -11,79 +9,22 @@ interface IProps {
 }
 
 export const PlantDetails: FC<IProps> = memo(({ plant }) => {
-  const backgroundImgCss = useMemo(() => {
-    const imgUrl = `/images/bio/${plant.name.toLowerCase().split(' ').join('-')}.png`;
-    return css({
-      background: `#3E4357 url(${imgUrl}) no-repeat center center`,
-      backgroundSize: 'contain',
-    });
-  }, [plant.name]);
+  const imgUrl = useMemo(
+    () => `/images/bio/${plant.name.toLowerCase().split(' ').join('-')}.png`,
+    [plant.name],
+  );
 
   return (
-    <div>
-      <div css={headingCss}>
-        <div css={[imageCss, backgroundImgCss]} />
-        <div css={headingContentCss}>
-          <Typography variant="h6">{plant.name}</Typography>
-          <Grid container>
-            <Grid item xs={6}>
-              <Typography variant="body1" css={titleCss}>
-                <small>Growth Rate</small>
-                <br />
-                {plant.growthRate.value} {plant.growthRate.rate}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="body1" css={titleCss}>
-                <small>Yield</small>
-                <br />
-                {plant.yield}
-              </Typography>
-            </Grid>
-          </Grid>
-        </div>
-      </div>
-      <div css={contentCss}>
-        <Typography variant="subtitle1" css={titleCss}>
-          Inputs
-        </Typography>
+    <DetailsBase title={plant.name} imgUrl={imgUrl}>
+      <DetailsSection title="Growth Rate">
+        {plant.growthRate.value} {plant.growthRate.rate}
+      </DetailsSection>
+      <DetailsSection title="Yield">{plant.yield}</DetailsSection>
+      <DetailsSection title="Inputs">
         <ResourceChips ios={plant.inputs} />
-      </div>
-    </div>
+      </DetailsSection>
+    </DetailsBase>
   );
 });
-
-const headingCss = css({
-  display: 'flex',
-  flexWrap: 'wrap',
-});
-
-const headingContentCss = (theme: Theme) =>
-  css({
-    paddingTop: theme.spacing(3),
-    paddingRight: theme.spacing(3),
-    paddingLeft: theme.spacing(3),
-    paddingBottom: 0,
-    width: 500 - 160,
-    flexGrow: 1,
-  });
-
-const imageCss = css({
-  width: 120,
-  height: 120,
-});
-
-const titleCss = (theme: Theme) =>
-  css({
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(),
-  });
-
-const contentCss = (theme: Theme) =>
-  css({
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-  });
 
 export default PlantDetails;
