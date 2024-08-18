17511c2bea23a4515916dd07ab1be7e907c5c13a
David Lee <devdavidlee@gmail.com>
2024-08-18 12:15:40 -0700

    Fixing styles check

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,9 +18,7 @@ import { FC, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 export const App: FC = () => {
-  const [state, { getTheme }
-    
-  ] = useContext();
+  const [state, { getTheme }] = useContext();
   const { theme } = state;
 
   useEffect(() => {
